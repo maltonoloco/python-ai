9ca98832d9bdac3fa8926488499d6121d0337de1
malte <malte.kottmann@ruhr-uni-bochum.de>
2023-03-31 12:45:17 +0200

    new test-cases

--- test/test_cases.py
+++ test/test_cases.py
@@ -1,104 +1,76 @@
 import sys, os
 from src import ecc_operations as ECC
+import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-def test_double():
-    ecc = ECC.ECC(2, 2, 17)
-    p = ECC.Point(5, 1)
-    if ecc.double(p) == ECC.Point(6, 3):
-        print("valid")
-    else:
-        print("not valid")
-
-    p = ECC.Point(6, 3)
-    if ecc.double(p) == ECC.Point(3, 1):
-        print("valid")
-    else:
-        print("not valid")
+def test_valid_elliptic_curve():
+    ec = ECC.ECC(2, 2, 17)
+    with pytest.raises(ValueError):
+        ec2 = ECC.ECC(2, 2, 18)
+    with pytest.raises(ValueError):
+        ec3 = ECC.ECC(5, 16, 17)
     
-    p = ECC.Point(3, 1)
-    if ecc.double(p) == ECC.Point(13, 7):
-        print("valid")
-    else:
-        print("not valid")
 
 
 def test_add():
     ecc = ECC.ECC(2, 2, 17)
     p = ECC.Point(5, 1)
     q = ECC.Point(6, 3)
-    if ecc.add(p, q) == ECC.Point(10, 6):
-        print("valid")
-    else:
-        print("not valid")
-    
-    q = ECC.Point(9, 16)
-    if ecc.add(p, q) == ECC.Point(16, 13):
-        print("valid")
-    else:
-        print("not valid")
-    
+    assert(ecc.add(p, q) == ECC.Point(10, 6))
     q = ECC.Point(5, 16)
-
-    if ecc.add(p, q) == ECC.Point(0, 0, True):
-        print("valid")
-    else:
-        print("not valid")
-
-def test_ord_ECC():
-    ecc = ECC.ECC(2, 2, 17)
-    if ecc.ord_ECC() == 19:
-        print("valid")
-    else:
-        print("not valid")
+    assert(ecc.add(p, q) == ECC.Point(0, 0, True))
+    q = ECC.Point(5, 2)
+    with pytest.raises(ValueError):
+        ecc.add(p, q)
     
 
 def test_ord_Point():
     ecc = ECC.ECC(2, 2, 17)
     p = ECC.Point(5, 1)
-    if p.ord_Point(ecc) == 19:
-        print("valid")
-    else:
-        print("not valid")
+    assert(p.ord_Point(ecc) == 19)
+
+    p = ECC.Point(16, 14)
+    with pytest.raises(ValueError):
+        p.ord_Point(ecc)
 
 def test_point_check():
     ecc = ECC.ECC(2, 2, 17)
+
     p = ECC.Point(5, 1)
-    if p.check(ecc):
-        print("valid")
-    else:
-        print("not valid")
-    
+    assert(p.check(ecc))
+
     p = ECC.Point(3, 2)
-    if not p.check(ecc):
-        print("valid")
-    else:
-        print("not valid")
-    
+    assert(not p.check(ecc))
+
     p = ECC.Point(0, 0, True)
-    if p.check(ecc):
-        print("valid")
-    else:
-        print("not valid")
-    
+    assert(p.check(ecc))
 
 def test_get_inverted():
     ecc = ECC.ECC(2, 2, 17)
+
     p = ECC.Point(5, 1)
-    if p.get_inverted(ecc) == ECC.Point(5, 16):
-        print("valid")
-    else:
-        print("not valid")
+    assert(p.get_inverted(ecc) == ECC.Point(5, 16))
     
     p = ECC.Point(16, 13)
-    if p.get_inverted(ecc) == ECC.Point(16, 4):
-        print("valid")
-    else:
-        print("not valid")
+    assert(p.get_inverted(ecc) == ECC.Point(16, 4))
     
+    p = ECC.Point(16, 14)
+    with pytest.raises(ValueError):
+        p.get_inverted(ecc)
+
     p = ECC.Point(0, 0, True)
-    if p.get_inverted(ecc) == ECC.Point(0, 0, True):
-        print("valid")
-    else:
-        print("not valid")
+    assert(p.get_inverted(ecc) == ECC.Point(0, 0, True))
+
+def test_double():
+    ecc = ECC.ECC(2, 2, 17)
+    
+    p = ECC.Point(5, 1)
+    assert(ecc.double(p) == ECC.Point(6, 3))
+
+    p = ECC.Point(16, 14)
+    with pytest.raises(ValueError):
+        ecc.double(p)
+
+    p = ECC.Point(0, 0, True)
+    assert(ecc.double(p) == ECC.Point(1, 1, True))
